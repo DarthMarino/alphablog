32cb0cdbe68d0ac975b3c21da46a8487f482fe32
Marino <marinogomez24@gmail.com>
2021-04-25 15:28:21 -0400

    Added some componets: -For the posts -For the profiles
    Also implemented some functions for the Firebase

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,3 +1,34 @@
-export default function Profile({}) {
-  return <main>User Profile</main>;
+import PostFeed from "../../components/postFeed";
+import UserProfile from "../../components/userProfile";
+import { getUserWithUsername, postToJSON } from "../../lib/firebase";
+
+export async function getServerSideProps({ query }) {
+  const { username } = query;
+  const userDoc = await getUserWithUsername(username);
+
+  // JSON serializable data
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsQuery = userDoc.ref
+      .collection("posts")
+      .where("published", "==", true)
+      .orderBy("createdAt", "desc")
+      .limit(5);
+    posts = (await postsQuery.get()).docs.map(postToJSON);
+  }
+  return {
+    props: { user, posts }, //will be passed to the page component as props
+  };
+}
+
+export default function ProfilePage({ user, posts }) {
+  return (
+    <main>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
+    </main>
+  );
 }
